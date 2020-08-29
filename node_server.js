601c124ca93a0cc57ead2de69abae2065ae0c282
Miguelnz30 <miguelcanales72@hotmail.com>
2020-08-28 19:08:44 -0600

    arreglo css

--- node_server.js
+++ node_server.js
@@ -1,12 +1,37 @@
 var http = require('http');
 var fs = require('fs');
+const path = require("path");
+http.createServer((request, response)=> {
 
-http.createServer(function (req, res) {
-  fs.readFile('index3.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+           let filePath = request.url;
+    if (filePath == '/') {
+        filePath = __dirname+'/index.html';
+    }
+    fileExtension= path.extname(filePath);
+
+        fs.readFile(filePath,{encoding:"UTF-8"}, (error,content)=>{
+            fileType = path.extname(filePath);
+            if(!error) {
+                switch (fileType) {
+                    case ".css":
+                        response.writeHead(200, {"Content-Type": "text/css"});
+                        response.write(content);
+                    break;
+                    case ".js":
+                        response.writeHead(200, {"Content-Type": "text/javascript"});
+                        response.write(content);
+                    break;
+                    default:
+                        response.writeHead(200, {"Content-Type": "text/html"});
+                        response.write(content);
+                }
+                response.end();
+            } else {
+                response.writeHead(404, {"Content-Type": "text/html"});
+                response.write("error file");
+                response.end(error);
+            }
+        })
+    }).listen(9000);
 console.log('Server is running');
-    
+
